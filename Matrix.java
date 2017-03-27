0e33d7a7840c5118a759089069aeeb35d0071971
nrbaker <nbakerkc11@gmail.com>
2017-03-26 23:32:04 -0500

    Added the getMatrixAtIndex and determinant methods
    These methods will be working closely with the main function of the program. Additional comments have been made on the functions themselves

--- Matrix.java
+++ Matrix.java
@@ -15,5 +15,35 @@ public class Matrix
 		//set the array parameter to the private member variable.
 		mdata = arr;
 	}
+	public double getMatrixAtIndex(int a, int b)
+	{
+		//return the array at the given index
+		return mdata[a][b];
+	}
+	public double determinant()
+	{
+		
+		double det = 0.0;
+		//if size of array is 1 do this code
+		if(number == 1)
+		{
+			det = mdata[0][0];
+		}
+		//if size of array is 2 do this code
+		else if(number == 2)
+		{
+			det = mdata[0][0] * mdata[1][1] - mdata[0][1] * mdata[1][0];
+		}
+		else
+		{
+			for(int i = 0; i < number ; ++i)
+			{
+				det += Math.pow(-1.0, (double)i) * mdata[0][i]*subMatrix(0, i).determinant();
+			}
+		}
+		
+		return det;//returns the double
+		
+	}
  }
 
