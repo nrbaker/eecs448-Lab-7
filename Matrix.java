7f099aa4789cd46d3998f62b1d654af8f95eb92d
nrbaker <nbakerkc11@gmail.com>
2017-03-26 23:29:19 -0500

    Created matrix.java file
    Created the constructor function along with private member variables.

---
+++ Matrix.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import static java.lang.Math.pow;
+public class Matrix 
+{
+	private int MaxSize = 10;
+	private int number;
+	private double mdata[][] = new double[MaxSize][MaxSize];
+	public Matrix(int myNum)//constructor
+	{
+		MaxSize = myNum;
+		number = myNum;
+	}
+ }
